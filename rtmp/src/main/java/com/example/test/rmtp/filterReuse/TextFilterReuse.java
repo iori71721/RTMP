3e00bd4cf261ef1fcb06d7d7fc93815e0893f5b5
iori <iori71721@gmail.com>
2020-05-11 19:26:57 +0800

    version 1.1.7 part 2 1.補上遺漏的code

--- rtmp/src/main/java/com/example/test/rmtp/filterReuse/TextFilterReuse.java
+++ rtmp/src/main/java/com/example/test/rmtp/filterReuse/TextFilterReuse.java
@@ -2,6 +2,7 @@ package com.example.test.rmtp.filterReuse;
 
 import android.graphics.PointF;
 import android.graphics.Typeface;
+import android.util.Log;
 
 import com.example.test.rmtp.filterReuse.record.ReuseTextFilterRecord;
 import com.pedro.encoder.input.gl.render.filters.object.TextObjectFilterRender;
@@ -16,19 +17,39 @@ public class TextFilterReuse extends BaseObjectFilterReuse<TextObjectFilterRende
     public TextObjectFilterRender generateFilter(ReuseTextFilterRecord record) {
         TextObjectFilterRender generateFilter=new TextObjectFilterRender();
         setupText(generateFilter,record);
-        generateFilter.setDefaultScale(record.getDefaultOutputSize().x,record.getDefaultOutputSize().y);
-        record.setScale(new PointF(generateFilter.getScale().x,generateFilter.getScale().y));
         generateFilter.setPosition(record.getPosition().x,record.getPosition().y);
         return generateFilter;
     }
 
-    private void setupText(TextObjectFilterRender textObjectFilterRender,ReuseTextFilterRecord textFilterRecord){
+    private PointF calcScale(PointF currentScale,PointF initScale){
+        float scaleX=currentScale.x/initScale.x;
+        float scaleY=currentScale.y/initScale.y;
+        return new PointF(scaleX,scaleY);
+    }
+
+    private void setupText(TextObjectFilterRender textObjectFilterRender, ReuseTextFilterRecord textFilterRecord){
+        PointF fixScale=null;
+        if(textFilterRecord.getInitScale() != null){
+                fixScale = calcScale(textFilterRecord.getScale(), textFilterRecord.getInitScale());
+        }
+
         if(textFilterRecord.typeface == null){
             textObjectFilterRender.setText(textFilterRecord.getText(),textFilterRecord.textSize,textFilterRecord.textColor);
         }else{
             textObjectFilterRender.setText(textFilterRecord.getText(),textFilterRecord.textSize,textFilterRecord.textColor
                     ,textFilterRecord.typeface);
         }
+
+        textObjectFilterRender.setDefaultScale(textFilterRecord.getDefaultOutputSize().x,textFilterRecord.getDefaultOutputSize().y);
+        textFilterRecord.setInitScale(textObjectFilterRender.getScale());
+
+        if(fixScale != null){
+            float fixScaleX=textFilterRecord.getInitScale().x*fixScale.x;
+            float fixScaleY=textFilterRecord.getInitScale().y*fixScale.y;
+            textObjectFilterRender.setScale(fixScaleX,fixScaleY);
+        }
+
+        textFilterRecord.setScale(new PointF(textObjectFilterRender.getScale().x,textObjectFilterRender.getScale().y));
     }
 
     public void setText(String text) {
@@ -49,13 +70,14 @@ public class TextFilterReuse extends BaseObjectFilterReuse<TextObjectFilterRende
         getFilterRecord().textColor=textColor;
         getFilterRecord().typeface=typeface;
         setupText(getReusedFilter(),getFilterRecord());
-
-        reuse(attachCamera);
+        getFilterRecord().updateInformation(getReusedFilter(),isVisible());
     }
 
     @Override
     public void reuse(RtmpCamera2 attachCamera) {
-        TextObjectFilterRender reusedFilter=generateFilter(getFilterRecord());
+        TextObjectFilterRender reusedFilter=new TextObjectFilterRender();
+        setupText(reusedFilter,getFilterRecord());
+        reusedFilter.setPosition(getFilterRecord().getPosition().x,getFilterRecord().getPosition().y);
         setReusedFilter(reusedFilter);
         attachCameraAndRender(attachCamera,getReusedFilter(),getFilterRecord().getReuseIndex());
     }
